fa8997e87541dd750b1241b1fb82847df1596a56
Silas Silva <silvasilas66@gmail.com>
2021-07-10 12:01:59 -0300

    Connecting with MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
+
+mongoose
+ 	.connect(process.env.MONGO_URL, {
+    	useNewUrlParser: true,
+    	useUnifiedTopology: true,
+    	useCreateIndex: true,
+    	useFindAndModify:true
+  	})
+  	.then(console.log("Connected to DataBase sucessfully!"))
+  	.catch((err) => console.log(err));
 
 app.use('/', (req, res) => res.send({ message: 'Welcome to my blog, user' }));
 app.listen(5000, () => console.log('Server is running on port 5000.'));
